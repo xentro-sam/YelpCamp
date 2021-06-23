f16b3c95984d53a71633c7dd0241785e3304be91
Shivam Agarwal <itisshivam22@gmail.com>
2021-06-23 15:29:31 +0530

    Create a separate dir to serve static assets. We also set useFindAndModify to false for mongoose to avoid the deprecated warning.

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser : true,
     useCreateIndex : true,
-    useUnifiedTopology : true
+    useUnifiedTopology : true,
+    useFindAndModify : false
 });
 
 const db = mongoose.connection;
@@ -30,6 +31,7 @@ app.set('views',path.join(__dirname,'views'));
 
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
 
 const validateCampground = (req,res,next) => {
     const {error} = campgroundSchema.validate(req.body);
