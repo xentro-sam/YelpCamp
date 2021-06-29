f8f116eb183cd69ea7e1933d46e00466c27d0893
Shivam Agarwal <itisshivam22@gmail.com>
2021-06-29 16:54:12 +0530

    Add the multer middleware from npm. To parse the data from the form when files are submitted ie, enctype="multipart/form-data" is used.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn,isAuthor,validateCampground} = require('../middleware');
+const multer = require('multer');
+const upload = multer({dest : 'uploads/'});
 
 const Campground = require('../models/campground');
 
 
 router.route('/')
     .get(catchAsync (campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync (campgrounds.createCampground));
+    //.post(isLoggedIn, validateCampground, catchAsync (campgrounds.createCampground));
+    .post(upload.array('image'), (req,res) => {
+        res.send(req.body, req.files);
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
