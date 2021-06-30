a9e61203de3749bfb1c77e90809345f3d8d97b9f
Shivam Agarwal <itisshivam22@gmail.com>
2021-06-30 12:25:48 +0530

    Fix our seeds

--- seeds/index.js
+++ seeds/index.js
@@ -26,9 +26,14 @@ const seedDB = async () => {
             author : '60d34e51ad28d431312075c0',
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
             title : `${sample(descriptors)} ${sample(places)}`,
-            image : 'https://source.unsplash.com/collection/483251',
             description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusamus, temporibus itaque dignissimos numquam autem ab tempore eligendi corporis culpa cupiditate exercitationem, rerum iste dolore deserunt vel praesentium aliquid id.',
-            price
+            price,
+            images : [
+                {
+                    url : 'https://res.cloudinary.com/xentro-sam/image/upload/v1625033794/YelpCamp/ktevlhy6jkobmtmiorqc.jpg',
+                    filename : 'YelpCamp/ktevlhy6jkobmtmiorqc'
+                }
+            ]
         });
         await camp.save();
     }
