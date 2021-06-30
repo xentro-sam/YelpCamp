6e42b3054c5606888df0fa1ce7a20e1291c62121
Shivam Agarwal <itisshivam22@gmail.com>
2021-06-30 11:48:41 +0530

    Store Uploaded Image links in Mongo.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,10 +12,7 @@ const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync (campgrounds.index))
-    //.post(isLoggedIn, validateCampground, catchAsync (campgrounds.createCampground));
-    .post(upload.array('image'), (req,res) => {
-        res.send(req.body, req.files);
-    })
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync (campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
