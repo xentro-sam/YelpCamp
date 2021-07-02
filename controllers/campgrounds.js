16619d61991fce25321866cb28bc351cbdb84216
Shivam Agarwal <itisshivam22@gmail.com>
2021-07-02 08:54:32 +0530

    Work with GeoJSON to add coordinates to database. Done to add coordinates of entered location to an entry in campground collection of our mongo database.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -14,8 +14,12 @@ module.exports.renderNewForm = (req,res) => {
 };
 
 module.exports.createCampground = async (req,res,next) => {
-    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const geoData = await geocoder.forwardGeocode({
+        query : req.body.campground.location,
+        limit : 1
+    }).send();
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({ url : f.path, filename : f.filename }));
     campground.author = req.user._id;
     await campground.save();
