3b6c93c5bcb148d1fcc7dc8eced1898df0bde24f
Shivam Agarwal <itisshivam22@gmail.com>
2021-07-04 00:55:00 +0530

    Do minor changes to session/cookies.

--- app.js
+++ app.js
@@ -47,11 +47,13 @@ app.use(mongoSanitize({
 }));
 
 const sessionConfig = {
+    name : 'session',
     secret : 'thisshouldbeabettersecret!',
     resave : false,
     saveUninitialized : true,
     cookie : {
         httpOnly : true,
+        //secure : true,
         expires : Date.now() + 1000*60*60*24*7,
         maxAge : 1000*60*60*24*7
     }
